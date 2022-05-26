e4d5dc59a5c68063a1c7b0a4e7076493c913a836
profoundA <81711479+profoundA@users.noreply.github.com>
2022-05-26 18:51:04 +0300

    Initial Commit

---
+++ PryanikyTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PryanikyTest
+//
+//  Created by Andrey Lobanov on 26.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
